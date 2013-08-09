d6e7480a26ca2fc8272fc420aa21e472172ae453
Ron Valstar <sjeiti@gmail.com>
2013-08-09 21:51:56 +0200

    remove unused function declaration arguments and contents

--- Gruntfile.js
+++ Gruntfile.js
@@ -87,14 +87,12 @@ module.exports = function (grunt) {
 		},
 
 		testUsage: {
-			options: { foo: 'bar' },
+			options: {},
 			emmet: {
 				src: './temp/emmet.js',
 				dest: './temp/emmet.min.js',
-				prepare: function(){
-
-				},
 				append: '\n;GLOBAL.emmet=emmet;',
+				prepare: function(){},
 				test: function(){
 					[
 						'ul>li*3>a*2',
@@ -125,27 +123,43 @@ module.exports = function (grunt) {
 
 
 	grunt.registerMultiTask('testUsage', '', function() {
-		var i
+		var oOptions = this.options({
+					disable: true
+					,mark: 'UNUSED'
+					,uglify: true
+					,uglifyOptions: null // see: https://github.com/mishoo/UglifyJS2#compressor-options
+					,replace: true
+				})
+			,i
 			,iLineNr
 			,sLine
-			,oData = this.data
-			,sFile = fs.readFileSync(oData.src).toString()
-			,aLines = sFile.split(/[\n]/g)
-			,iLines = aLines.length
-			,sMark = 'return false;/*UNUSED*/'
+			,sFile// = fs.readFileSync(oData.src).toString()
+			,aLines// = sFile.split(/[\n]/g)
+			,iLines// = aLines.length
+			,sMark = (oOptions.disable?('return false;'):'')+(oOptions.mark!==''?'/*'+oOptions.mark+'*/':'')
 			,rFunctionG = /function\s?(\w*)?\(([^\)]+)?\)\s?{/g
 			,aFnLines = []
 			,iFnLines
-			,sAppend = oData.append||''
+			,sAppend// = oData.append||''
 			,sNewFile
-			//
+			// sniff injection vars
 			,sInsertName = 'sniff_'+Math.ceil(1E8*Math.random()).toString(36)
 			,oCountPrints = {}
 			,iUnused
 			,aUnused = []
 			,aUsed = []
+			// dummy replacement vars
+			,sLargeDummy = 'z'+Math.ceil(1E32*Math.random()).toString(36)
+			,iDummyLength = 1
+			,iDummyIndex = 0
+			,sDummyName
+			// uglify vars
+			,bUglify = oOptions.uglify
+			,uglify
+			,oCompressor
+			,oAst
 		;
-		///
+		//
 		function getInsert(nr){
 			return ';'+sInsertName+'('+nr+');';
 		}
@@ -153,73 +167,92 @@ module.exports = function (grunt) {
 			oCountPrints[line]++;
 		};
 		//
-		// inject sniffer function into target script
-		for (i=0;i<iLines;i++) {
-			iLineNr = i+1;
-			sLine = aLines[i];
-			if (sLine.match(rFunctionG)) {
-				aFnLines.push(iLineNr);
-				aLines[i] = sLine = sLine.replace(rFunctionG,'$&'+getInsert(iLineNr));
+		// Iterate over all src-dest file pairs.
+		this.files.forEach(function(f) {
+			sFile = fs.readFileSync(f.src).toString();
+			sAppend = f.append||'';
+			aLines = sFile.split(/[\n]/g);
+			iLines = aLines.length;
+			//
+			// inject sniffer function into target script
+			for (i=0;i<iLines;i++) {
+				iLineNr = i+1;
+				sLine = aLines[i];
+				if (sLine.match(rFunctionG)) {
+					aFnLines.push(iLineNr);
+					aLines[i] = sLine = sLine.replace(rFunctionG,'$&'+getInsert(iLineNr));
+				}
 			}
-		}
-		iFnLines = aFnLines.length;
-		sNewFile = aLines.join('\n')+sAppend;
-		fs.writeFileSync(oData.dest,sNewFile);
-		//
-		// count unused functions
-		oData.prepare&&oData.prepare();
-		for (i=0;i<iLines;i++) oCountPrints[aFnLines[i]] = 0;
-		require(oData.dest);
-		oData.test&&oData.test();
-		//
-		// gather result data
-		for (i=0;i<iFnLines;i++) {
-			iLineNr = aFnLines[i];
-			if (oCountPrints[iLineNr]===0) {
-				aUnused.push(iLineNr);
+			iFnLines = aFnLines.length;
+			sNewFile = aLines.join('\n')+sAppend;
+			fs.writeFileSync(f.dest,sNewFile);
+			//
+			// count unused functions
+			f.prepare&&f.prepare();
+			for (i=0;i<iLines;i++) oCountPrints[aFnLines[i]] = 0;
+			require(f.dest);
+			f.test&&f.test();
+			//
+			// gather result data
+			for (i=0;i<iFnLines;i++) {
+				iLineNr = aFnLines[i];
+				if (oCountPrints[iLineNr]===0) {
+					aUnused.push(iLineNr);
+				} else {
+					aUsed.push(iLineNr);
+				}
+			}
+			iUnused = aUnused.length;
+			//
+			// remove sniff() and disable unused functions
+			for (i=0;i<iFnLines;i++) {
+				iLineNr = aFnLines[i];
+				sLine = aLines[iLineNr-1];
+				var bUsed = aUsed.indexOf(iLineNr)>=0
+					,sInsert = getInsert(iLineNr)
+				;
+				aLines[iLineNr-1] = sLine.replace(sInsert,bUsed?'':sMark);
+			}
+			sNewFile = aLines.join('\n');
+			//
+			// let uglify compress it
+			if (bUglify) {
+				uglify = require('uglify-js');
+				oCompressor = uglify.Compressor(oOptions.uglifyOptions||{warnings:false});
+				oAst = uglify.parse(sNewFile);
+				oAst.figure_out_scope();
+				oAst = oAst.transform(oCompressor);
+				oAst.figure_out_scope();
+				oAst.compute_char_frequency();
+				oAst.mangle_names();
+				sNewFile = oAst.print_to_string({});//options
+				//
+				// replace unused function expressions with dummy, alter unused function declarations
+				if (oOptions.replace) {
+					// find smallest possible dummy name
+					while (iDummyIndex>=0) {
+						iDummyLength++;
+						iDummyIndex = sNewFile.indexOf(sLargeDummy.substr(0,iDummyLength));
+					}
+					sDummyName = sLargeDummy.substr(0,iDummyLength);
+					// replace unused function expressions with dummy
+					sNewFile = 'function '+sDummyName+'(){return!1};'+sNewFile.replace(/function\([^)]*\){return!1}/g,sDummyName);
+					// remove unused function declaration arguments and contents
+					sNewFile = sNewFile.replace(/(function\s\w+\()([^)]*)(\){)(return!1)(})/g,'$1$3$5');
+				}
+			}
+			//
+			// save result
+			fs.writeFileSync(f.dest,sNewFile);
+			//
+			// result log
+			grunt.log.writeln('tested',f.src,'with',iLines,'lines');
+			if (iUnused===0) {
+				grunt.log.ok('no redundant functions found');
 			} else {
-				aUsed.push(iLineNr);
+				grunt.log.error(iUnused+' of '+iFnLines+' functions unused');
 			}
-		}
-		iUnused = aUnused.length;
-		//
-		// fix unused functions
-		for (i=0;i<iFnLines;i++) {
-			iLineNr = aFnLines[i];
-			sLine = aLines[iLineNr-1];
-			var bUsed = aUsed.indexOf(iLineNr)>=0
-				,sInsert = getInsert(iLineNr)
-			;
-			aLines[iLineNr-1] = sLine.replace(sInsert,bUsed?'':sMark);
-		}
-		sNewFile = aLines.join('\n');
-		//
-		// let uglify compress it
-		var uglify = require('uglify-js');
-		var toplevel = uglify.parse(sNewFile);
-		toplevel.figure_out_scope();
-		var compressor = uglify.Compressor({warnings:false});//options
-		var compressed_ast = toplevel.transform(compressor);
-		compressed_ast.figure_out_scope();
-		compressed_ast.compute_char_frequency();
-		compressed_ast.mangle_names();
-		sNewFile = compressed_ast.print_to_string({});//options
-		//
-		// replace all function(a,b,c){return!1} occurrences with a dummy
-		var rFnNotOne = /function\([^)]*\){return!1}/g
-			,sDummyName = 'z'+Math.ceil(1E8*Math.random()).toString(36);
-		sNewFile = 'function '+sDummyName+'(){return!1};'+sNewFile.replace(rFnNotOne,sDummyName);
-		//
-		// save result
-		fs.writeFileSync(oData.dest,sNewFile);
-		//
-		// result log
-		grunt.log.writeln('tested',oData.src,'with',iLines,'lines');
-		if (iUnused===0) {
-			grunt.log.ok('no redundant functions found');
-		} else {
-			grunt.log.error(iUnused+' of '+iFnLines+' functions unused');
-		}
+		});
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
